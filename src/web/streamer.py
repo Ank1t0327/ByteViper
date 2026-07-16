83e824b5df32b0543f8c61590ac3b92c674c65fc
Ank1t0327 <ank1t032718@gmail.com>
2026-07-16 09:57:25 +0530

    feat(streamer): integrate raw packet buffer in PacketStreamer

--- src/web/streamer.py
+++ src/web/streamer.py
@@ -5,6 +5,7 @@ class PacketStreamer:
     def __init__(self, max_history=2000):
         self.packets = deque(maxlen=max_history)
         self.alerts = deque(maxlen=max_history)
+        self.raw_packets = deque(maxlen=max_history)
         self.lock = threading.Lock()
 
     def add_packet(self, packet_data):
@@ -26,11 +27,20 @@ class PacketStreamer:
     def get_alerts_since(self, timestamp=0.0):
         with self.lock:
             return [a for a in self.alerts if a.get('timestamp', 0) > timestamp]
+
+    def add_raw_packet(self, raw_pkt):
+        with self.lock:
+            self.raw_packets.append(raw_pkt)
+
+    def get_raw_packets(self):
+        with self.lock:
+            return list(self.raw_packets)
     
     def clear(self):
         with self.lock:
             self.packets.clear()
             self.alerts.clear()
+            self.raw_packets.clear()
 
 # Global streamer instance
 streamer = PacketStreamer()
